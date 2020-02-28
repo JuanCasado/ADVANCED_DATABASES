915171f3e41a6b7db6a4164691b07bf322a99d05
JuanCasado <juan.casado@edu.uah.es>
2020-02-28 20:31:20 +0100

    web

---
+++ web/build/precache-manifest.cd21a0c7a35d2dabdc0867f098887680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e60d186ca7b2635478c2179e90634a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2548af5a6e5f107fb7a",
+    "url": "/static/css/main.51d497f0.chunk.css"
+  },
+  {
+    "revision": "260542c24d0ad362dd0d",
+    "url": "/static/js/2.818d3adb.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.818d3adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2548af5a6e5f107fb7a",
+    "url": "/static/js/main.6238563f.chunk.js"
+  },
+  {
+    "revision": "cb9d30897eb8909eb05b",
+    "url": "/static/js/runtime-main.9dbd9737.js"
+  }
+]);
